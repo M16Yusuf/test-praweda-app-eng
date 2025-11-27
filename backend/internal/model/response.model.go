72fba13f39a4745cf29346e7c37cadbf08b7fb0b
M16yusuf <yusufsmd58@gmail.com>
2025-11-27 23:37:59 +0700

    refactor: remove redundant ErrorResponse struct and update Response model

--- backend/internal/model/response.model.go
+++ backend/internal/model/response.model.go
@@ -4,12 +4,8 @@ type Response struct {
 	IsSuccess bool   `json:"is_success"  example:"true"`
 	Code      int    `json:"code,omitempty"  example:"200"`
 	Page      int    `json:"page,omitempty"  example:"1"`
+	Err       string `json:"error,omitempty" example:"Error message..."`
 	Msg       string `json:"message,omitempty"  example:"Example message success..."`
-}
-
-type ErrorResponse struct {
-	Response
-	Err string `json:"error" example:"Error message..."`
 }
 
 type ResponseData struct {
