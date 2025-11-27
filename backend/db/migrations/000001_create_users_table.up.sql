5d8ee216fba400b5199cd631d2a355fa4005e7ff
M16yusuf <yusufsmd58@gmail.com>
2025-11-27 23:36:37 +0700

    feat: set default value for user ID to generate random UUID

--- backend/db/migrations/000001_create_users_table.up.sql
+++ backend/db/migrations/000001_create_users_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
