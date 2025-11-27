27430713f0288ed855314ba91f82dfcd2efabfd9
M16yusuf <yusufsmd58@gmail.com>
2025-11-27 23:38:09 +0700

    feat: add CORS middleware for handling cross-origin requests

---
+++ backend/internal/middleware/cors.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"net/http"
+	"slices"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware(ctx *gin.Context) {
+	// setup whitelist origin
+	whitelist := []string{"http://localhost:5173", "http://127.0.0.1:5173"}
+	origin := ctx.GetHeader("Origin")
+	if slices.Contains(whitelist, origin) {
+		ctx.Header("Access-Control-Allow-Origin", origin)
+	}
+	// header untuk preflight cors
+	ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+	ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Type")
+	// tangani apabila bertemu preflight
+	if ctx.Request.Method == http.MethodOptions {
+		// ctx.Header("X-DEBUG", "preflight-handled")
+		ctx.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+	// ctx.Header("X-DEBUG", "actual request")
+	ctx.Next()
+}
