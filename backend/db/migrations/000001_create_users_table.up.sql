524d0bf06262cd2435321d57a2de5c986bdc5b46
M16yusuf <yusufsmd58@gmail.com>
2025-11-28 00:23:39 +0700

    feat: update migration scripts to create users_task2 table

--- backend/db/migrations/000001_create_users_table.up.sql
+++ backend/db/migrations/000001_create_users_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users_task2 (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
