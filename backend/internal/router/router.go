62a44c1393ff3c5fc8559506794014233ae12b49
M16yusuf <yusufsmd58@gmail.com>
2025-11-27 21:59:52 +0700

    feat: add router initialization with CORS middleware and 404 handling

---
+++ backend/internal/router/router.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"net/http"
+	"test-praweda-app-eng/internal/middleware"
+	"test-praweda-app-eng/internal/model"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitRouter(db *pgxpool.Pool) *gin.Engine {
+	router := gin.Default()
+	router.Use(middleware.CORSMiddleware)
+
+	// make directori public accesible
+	router.Static("/img", "public")
+
+	InitAuthRouter(router, db)
+
+	// handle not found route
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, model.Response{
+			IsSuccess: false,
+			Code:      http.StatusNotFound,
+			Msg:       "Page not found!",
+		})
+	})
+
+	return router
+}
